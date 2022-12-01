994349960690e8b5ca6d4b5b64200d12cebcb167
Denys <DenysYuryev@gmail.com>
2022-12-01 23:10:12 +0200

    localStorage

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,2 +1,36 @@
 'use strict';
 
+// мотоди localStorage
+/*
+localStorage.setItem('num', 5);
+localStorage.getItem('num');
+
+localStorage.remmoveItem('num');
+localStorage.clear();
+*/
+
+const checkbox = document.querySelector('#checkbox'),   // id 
+      form = document.querySelector('form'),            // тег form (форма для звернення)
+      change = document.querySelector('#color');        //
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked =true;
+}
+
+if (localStorage.getItem('bgForm' === 'changed')) {
+    form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => localStorage.setItem('isChecked', true));
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bgForm') === 'changed') {
+        localStorage.removeItem('bgForm');
+        form.style.backgroundColor = '#fff';
+
+    } else {
+        localStorage.setItem('bgForm', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
