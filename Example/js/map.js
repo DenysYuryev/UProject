d3da034beed7e4510bfc8538c53725ff6b03a7f0
Denys <DenysYuryev@gmail.com>
2022-12-02 23:09:57 +0200

    update map (move to exemple), promise

--- Example/js/map.js
+++ Example/js/map.js
@@ -127,3 +127,23 @@ readMesages.add(messages[1]);
 readMesages.add(messages[0]);
 messages.shift();
 console.log(readMesages.has(messages[0]));
+
+// 
+
+const arr = [
+    {
+        name: 'Alex',
+        salary: 500
+    },
+    {
+        name: 'Ann',
+        salary: 1500
+    },
+    {
+        name: 'John',
+        salary: 2500
+    },
+];
+ 
+const result = arr.map(item => Object.entries(item)[1][1]).reduce((sum, curr) => sum + curr);
+console.log(result);
